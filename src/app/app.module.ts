93bb6ea89a29d19365dbb6dea726c3e6b3b7cabf
Knelia <knelia1989@gmail.com>
2023-12-06 10:50:14 -0500

    Componentes card, buttons y servicio share

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    ButtonsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
