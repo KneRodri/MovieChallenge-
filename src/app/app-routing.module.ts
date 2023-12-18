9006bc386826c536c555332a51c1a0a0b1a0659e
Knelia <knelia1989@gmail.com>
2023-12-18 11:27:13 -0500

    Ruta activa de home a movieDetails al seleccionar una película y la de retorno

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { MovieDetailsComponent } from './movie-details/movie-details/movie-details.component';
 
 const routes: Routes = [
+
   { path: '', redirectTo: 'app-home', pathMatch: 'full'},
   { path: 'app-home', component: HomeComponent },
-
+  { path: 'app-movie-details', component: MovieDetailsComponent}
 ];
 
 @NgModule({
