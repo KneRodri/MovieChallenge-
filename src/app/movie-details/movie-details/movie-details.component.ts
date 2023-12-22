ae640142d42daee8482be3a265ceea7b770eb37b
Knelia <knelia1989@gmail.com>
2023-12-21 23:43:30 -0500

    Se agregan detalles de la películas y funciones

--- src/app/movie-details/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details/movie-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShareService } from 'src/app/share/share.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,21 +8,48 @@ import { ShareService } from 'src/app/share/share.service';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
+  id: number = 0;
   title: string = '';
   vote_count: number = 70;
   releaseYear: string = '';
   genres: any = {};
   details: any = {};
-  img_url: string = 'https://image.tmdb.org/t/p/w500/';
+  imgs_url: string = 'https://image.tmdb.org/t/p/w500/';
   backdrop_img: string = '';
-  // genresStrings: any = [];
-  // movieGenres: any[] = [];
+  genresStrings: any = [];
+  movieGenres: any[] = [];
 
   constructor(
     private shareService: ShareService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.getDetailsById();
   }
 
+  private getDetailsById(): void {
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+      console.log(this.id, 'hola id');
+      this.fillDetails(this.id);
+    });
+  }
+
+  fillDetails(movieId: number) {
+    this.shareService.getMovieDetails(movieId).subscribe(data => {
+      console.log(data, 'hola data');
+      this.details = data;
+      console.log(this.details.genres, 'Hola generos');
+      this.movieGenres = this.details.genres
+    })
+  }
+
+  getBackdropImage() {
+    if (this.details) {
+      return this.imgs_url + this.details.backdrop_path;
+    }
+    return '';
+  }
 }
+
--- src/app/share/share.service.ts
+++ src/app/share/share.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { APIresponse } from '../models/models.component';
-// import { GenresList, Movie } from '../models/models.component';
+import { APIresponse, Movie } from '../models/models.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShareService {
+  
   private apiKey = 'bd2f3050b747335fd35c8ddd60f9bac8';
   private apiUrl = 'https://api.themoviedb.org/3/';
 
@@ -17,11 +17,12 @@ export class ShareService {
     return this.http.get<APIresponse>(`${this.apiUrl}movie/popular?api_key=${this.apiKey}`);
   }
 
-  // movieDetails(movieId: number) : Observable<Movie> {
-  //   return this.http.get<Movie>(`${this.apiUrl}/movie/${movieId}?api_key=${this.apiKey}`)
-  // }
+  getMovieDetails(movieId: number): Observable<Movie> {
+    return this.http.get<Movie>(`${this.apiUrl}movie/movie_id${movieId}?api_key=${this.apiKey}`);
+  }
+
+}
 
   // getGenresList(): Observable<GenresList> {
   //   return this.http.get<GenresList>(`${this.apiUrl}/genre/movie/list?api_key=${this.apiKey}`);
   // }
-}
